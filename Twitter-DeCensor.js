d280556fa367383a050f4a37e28b7fabe6db2e11
TehFridge <85436576+TehFridge@users.noreply.github.com>
2025-08-11 02:45:10 +0200

    Update Twitter-DeCensor.js

--- Twitter-DeCensor.js
+++ Twitter-DeCensor.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Twitter DeCensor
+// @name         Twitter DeCensor 
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  where my tweets at.
+// @version      1.1
+// @description  where my tweets at
 // @icon         https://barachlo.szprink.xyz/fiIAlnosGV5w.png
 // @match        https://twitter.com/*
 // @match        https://x.com/*
@@ -13,76 +13,79 @@
 (function() {
     'use strict';
 
-    // Helper to recursively remove mediaVisibilityResults key
-    function recursiveClean(obj) {
-        if (obj && typeof obj === "object") {
-            if ("mediaVisibilityResults" in obj) {
-                console.log("[PATCH] Removing mediaVisibilityResults from:", obj);
-                delete obj.mediaVisibilityResults;
-            }
-            for (const key in obj) {
-                recursiveClean(obj[key]);
-            }
-        }
-    }
+    const pageCode = `
+    (function() {
+        console.log("[DeCensor] Injected into page context");
 
-    // -------- Hook XHR --------
-
-    const realXHROpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url) {
-        this._url = url; // store URL for logging
-
-        this.addEventListener('readystatechange', () => {
-            if (this.readyState === 4 && this.responseType === '' && this.responseText) {
-                try {
-                    // Try parse JSON
-                    let json = JSON.parse(this.responseText);
-                    recursiveClean(json);
-                    const patched = JSON.stringify(json);
-
-                    // Override responseText and response with patched version
-                    Object.defineProperty(this, 'responseText', { writable: true });
-                    Object.defineProperty(this, 'response', { writable: true });
-                    this.responseText = this.response = patched;
-
-                    console.log(`[XHR PATCHED] ${this._url}`);
-
-                } catch (e) {
-                    // Not JSON, ignore
+        function recursiveClean(obj) {
+            if (obj && typeof obj === "object") {
+                if ("mediaVisibilityResults" in obj) {
+                    console.log("[PATCH] Removing mediaVisibilityResults from:", obj);
+                    delete obj.mediaVisibilityResults;
+                }
+                for (const key in obj) {
+                    recursiveClean(obj[key]);
                 }
             }
-        });
-        return realXHROpen.apply(this, arguments);
-    };
+        }
 
-    // -------- Hook fetch --------
+        // -------- Hook XHR --------
+        const realXHROpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function(method, url) {
+            this._url = url;
 
-    const realFetch = window.fetch;
-    window.fetch = async function(resource, init) {
-        const response = await realFetch(resource, init);
+            this.addEventListener('readystatechange', () => {
+                if (this.readyState === 4 && this.responseType === '' && this.responseText) {
+                    try {
+                        let json = JSON.parse(this.responseText);
+                        recursiveClean(json);
+                        const patched = JSON.stringify(json);
 
-        // Only patch Twitter API graphql calls
-        if (typeof resource === 'string' && resource.includes('/i/api/graphql/')) {
-            try {
-                const cloned = response.clone();
-                const data = await cloned.json();
+                        Object.defineProperty(this, 'responseText', { value: patched });
+                        Object.defineProperty(this, 'response', { value: patched });
 
-                recursiveClean(data);
+                        console.log(\`[XHR PATCHED] \${this._url}\`);
+                    } catch (e) {
+                        // ignore non-JSON
+                    }
+                }
+            });
+            return realXHROpen.apply(this, arguments);
+        };
 
-                const patchedBody = JSON.stringify(data);
+        // -------- Hook fetch --------
+        const realFetch = window.fetch;
+        window.fetch = async function(resource, init) {
+            const response = await realFetch(resource, init);
 
-                // Return a new Response with patched body, preserving original headers/status
-                return new Response(patchedBody, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: response.headers,
-                });
-            } catch (e) {
-                // JSON parse failed, fallback to original response
+            if (typeof resource === 'string' && resource.includes('/i/api/graphql/')) {
+                try {
+                    const cloned = response.clone();
+                    const data = await cloned.json();
+
+                    recursiveClean(data);
+
+                    const patchedBody = JSON.stringify(data);
+                    return new Response(patchedBody, {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers: response.headers,
+                    });
+                } catch (e) {
+                    return response;
+                }
+            } else {
                 return response;
             }
-        } else {
-            return response;
-        }
-    };
+        };
+    })();
+    `;
+
+    // Inject into page context via Blob URL (CSP-safe)
+    const blob = new Blob([pageCode], { type: "application/javascript" });
+    const url = URL.createObjectURL(blob);
+    const script = document.createElement('script');
+    script.src = url;
+    document.documentElement.appendChild(script);
+    script.remove();
 })();
